b665e76cca6ee91e29408d6f60e4428a45f18467
Akshi149 <akshita.ag321@gmail.com>
2021-07-01 18:22:36 +0530

    slight changes in UI

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import bg from '../../images/bg.svg';
 class Landing extends Component {
   render() {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
+            <br/><br/>
+            <img src={bg} alt='' style={{height:300}}/>
             <h4>
               <b>Built</b> a user-auth app with{" "}
               <span style={{ fontFamily: "monospace" }}>MERN</span> stack
@@ -36,7 +39,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
               >
                 Log In
               </Link>
